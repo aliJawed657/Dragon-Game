70550c9e1416ffb9b9f75e9b48a7c9b9d29ff3af
Ali Jawed <134846403+aliJawed657@users.noreply.github.com>
2023-07-24 18:14:38 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,68 @@
+score = 0;
+cross = true;
+
+
+document.onkeydown = function(e){
+    console.log("Key code is:" , e.keyCode)
+    if(e.keyCode==38){
+        dino = document.querySelector(".dino");
+        dino.classList.add("animateDino")
+        setTimeout(() => {
+            dino.classList.remove("animateDino")
+        } ,900);
+    }
+    if(e.keyCode==39){
+        dino = document.querySelector(".dino");
+        dinoX = parseInt(window.getComputedStyle(dino , null).getPropertyValue("left"));
+        dino.style.left = dinoX + 112+"px";
+   
+    }
+        if(e.keyCode==37){
+            dino = document.querySelector(".dino");
+            dinoX = parseInt(window.getComputedStyle(dino , null).getPropertyValue("left"));
+            dino.style.left = (dinoX - 112) +"px";
+}
+}
+
+setInterval(()=>{
+dino = document.querySelector(".dino")
+gameOver = document.querySelector(".gameOver")
+obstacle= document.querySelector(".obstacle")
+
+dx = parseInt(window.getComputedStyle(dino , null).getPropertyValue("left"));
+dy = parseInt(window.getComputedStyle(dino , null).getPropertyValue("top"));
+
+ox = parseInt(window.getComputedStyle(obstacle , null).getPropertyValue("left"));
+oy = parseInt(window.getComputedStyle(obstacle, null).getPropertyValue("top"));
+
+offsetX = Math.abs(dx - ox);
+offsetY = Math.abs(dy - oy);
+
+console.log(offsetX , offsetY)
+if(offsetX < 113 && offsetY <52){
+    gameOver.style.visibility = "visible"
+    obstacle.classList.remove("obstacleAni")
+}
+else if(offsetX < 145 && cross){
+    score+=1;
+    updateScore(score)
+    cross = false
+    setTimeout(() =>{
+        cross = true;
+    },1000)
+setTimeout(() =>{
+    aniDur = parseFloat(window.getComputedStyle(obstacle, null).getPropertyValue("animation-Duration"))
+    newDur = aniDur - 0.1;
+    obstacle.style.animationDuration = newDur + "s"
+
+},500)
+   
+}
+
+
+},10)
+
+
+function updateScore(score){
+    scoreCont.innerHTML = "YOUR SCORE : "  +score
+}
